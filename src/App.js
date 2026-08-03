1eacb167e73c622924b4318f8559e92554f6862d
Dharshini R <dharshinirsvd2007@email.com>
2026-08-03 10:34:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import ToDo from "./componenets/ToDo";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>TODO APP</h1>
+      {/* <header className="App-header"> */}
+       {/* <h1>TODO APP</h1> */}
+        <ToDo/>
+      {/* </header> */}
     </div>
   );
 }
